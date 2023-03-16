5cd054e18750d2c00db4b9df80d1cbd4fe5bc67c
Bingchao Zhao <47686010+Bingchao-Zhao@users.noreply.github.com>
2023-03-16 09:58:10 +0800

    Update test_wsi.py

--- test_wsi.py
+++ test_wsi.py
@@ -1,50 +1,84 @@
-from os import listdir
-import cv2
+
 import numpy as np
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 import torchvision.models as tm
-
+from torchvision import transforms
 from my_utils.utils import *
 import my_utils.handle_huge_img as hhi
 import my_utils.handle_img as hi
+import  copy
 import my_utils.file_util as fu
+import my_utils.handle_WSI as hw
+INPUT_PATH = 'input your data dir'
+SAVE_PATH = 'the dir to save segmentation mask'
+SAVE_COM_PATH = 'the dir to save concat image of segmentation mask and original image'
 
-def nor_big_patch(model, wsi_path):
-    # wsi_path should be a image. .png .jpg so on.
+def get_png_path(suffix='.png'):
+    ret = []
+    png_list = find_file(INPUT_PATH,3, suffix=suffix)
+    for p_p in png_list:
+        dir,n = os.path.split(p_p)
+        ret.append([p_p, dir, n])
+    return ret
+
+def get_img(img_path):
+    img = hi.cv2_reader(img_path)
+    return img
+
+def pipline(model, png_list):
+
+    for png_p, _dir, p_name in tqdm(png_list) :
+
+        split_path = fu.split_path(INPUT_PATH, _dir)
+        save_mask_path = os.path.join(SAVE_PATH, split_path, p_name)
+        save_concat_mask_path = os.path.join(SAVE_COM_PATH, split_path, p_name)
+        if just_ff(save_mask_path,file=True) and just_ff(save_concat_mask_path,file=True):
+            continue
+
+        img = get_img(png_p)[::2,::2,:]
+        img2 = copy.deepcopy(img)
+        mask = gen_mask(model, img)
+        ostu_mask = hw.ostu_seg_tissue(img)
+        mask = mask*(ostu_mask>0)
+        img2[mask<=0] = 0
+        mask_concat = hi.concat_img([img, img2],channel_frist=False)
+        just_dir_of_file(save_mask_path)
+        just_dir_of_file(save_concat_mask_path)
+        hi.cv2_writer(save_mask_path, mask)
+        hi.cv2_writer(save_concat_mask_path, mask_concat)
+
+def gen_mask(model, img):
     img_size=224
-    stride=200
-    batchsize = 128
-    wsi = hi.img_read(wsi_path)
-    indx_list = hhi.gen_patches_index(wsi.shape[0:2], img_size=img_size, \
+    stride=56
+    batchsize = 512
+    indx_list = hhi.gen_patches_index(img.shape[0:2], img_size=img_size, \
                                                 stride=stride, keep_last_size=True)
     indx_zip = MyZip(indx_list, batch=batchsize)    
-    pred_img, record_img = [], []
-
+    mask = np.zeros(img.shape[0:2], dtype=np.uint8)
+    transform = transforms.Compose([transforms.ToTensor()])
     with torch.no_grad():
         for num, ind in enumerate(indx_zip):
-            rtime_print("Processing {}/{}".format(num, len(indx_zip)))
-            ind = ind[0]
+            # rtime_print("Processing {}/{}".format(num, len(indx_zip)))
+            inds = ind[0]
             input_img_list = []
-            for _ind in ind:
-                input_img_list.append(torch.FloatTensor(hhi.gfi(wsi, _ind)))
-            input_img_list = torch.stack(input_img_list).cuda().permute(0,3,1,2)
+            for _ind in inds:
+                input_img_list.append(transform(hhi.gfi(img, _ind)))
+            input_img_list = torch.stack(input_img_list).cuda()
             pred = model(input_img_list).cpu().detach().numpy()
-            pred_img.extend(pred)
-            record_img.extend(np.ones(pred.shape))
+            for num, _ind in enumerate(inds):
+                if pred[num][0] > pred[num][1]:
+                    mask[_ind[0]:_ind[1], _ind[2]:_ind[3]] += 0 
+                else:
+                    mask[_ind[0]:_ind[1], _ind[2]:_ind[3]] += 1 
+    mask = np.array((mask>1)*255, dtype=np.uint8)
+    return mask
 
-    pred_img = hhi.Splice_patches_by_index(pred_img, indx_list, overlay_model='add')
-    record_img = hhi.Splice_patches_by_index(record_img, indx_list, overlay_model='add')
-    pred_img /= record_img
-    pred_img[pred_img<0] = 0
-    pred_img = pred_img.transpose(1,2,0)
-    np.array(pred_img*255, np.uint8)
-
-model       = tm.resnet34(pretrained=True)
+model       = tm.resnet34()
 model.fc    = nn.Linear(512 , 2)
 model.cuda().eval()
-pretrained_model = torch.load('weight/resnet34/epoch-10.pth') # 导入预训练权重
-model.load_state_dict(pretrained_model, strict=False) # 将与训练权重载入模型
-
-nor_big_patch(model, INPUT_PATH)
+pretrained_model = torch.load('weight path') # 导入预训练权重
+model.load_state_dict(pretrained_model) # 将与训练权重载入模型
+png_list = get_png_path()
+pipline(model, png_list)
